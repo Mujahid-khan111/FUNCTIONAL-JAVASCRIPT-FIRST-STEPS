244b330c33d80edd65ed6c0118e0e1c04515abcc
mujahid-khan111 <khanmujahid7003@gmail.com>
2024-10-20 01:34:15 +0530

    Functional-Javascript

---
+++ js/1-Pure-and-impure-function.js
@@ -0,0 +1,31 @@
+
+// // impure function 
+// let name = "mujahid";
+
+// function greet(){
+//     console.log(`Hello, ${name}!`);
+// }
+
+// greet(); // Hello, mujahid!
+
+// name = "mohammed";
+// greet(); // Hello, mohammed!
+
+
+// // Pure function
+// function greet(name){
+// console.log(`Hello,${name}!`);
+// }
+
+// greet("suleman"); 
+// greet("sahil");
+// greet("adnan");
+
+const arr = [1, 2, 3];
+
+function addToArray(element) {
+    arr.push(element);
+}
+console.log(element);
+
+addToArray();
